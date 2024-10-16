577d3236b124c130974765353f272f8fb5f22a79
av-TOR <alexvoloshintor@yandex.ru>
2024-10-16 22:39:41 +0200

    Add files via upload

---
+++ module_6_1 (2)..py
@@ -0,0 +1,56 @@
+class Animal:
+    def __init__(self, name,  alive=True, fed=False):
+        self.name = name
+        self.alive = alive
+        self.fed = fed
+
+    def eat(self, food):
+        if food.edible is True:
+            print(f"{self.name} съел {food.name}")
+            self.fed = True
+
+        else:
+            print(f"{self.name} не стал есть {food.name}")
+            self.alive = False
+
+
+class Plant:
+    def __init__(self, name, edible=False):
+        self.name = name
+        self.edible = edible
+
+
+class Mammal(Animal):
+    pass
+
+
+class Predator(Animal):
+    pass
+
+
+class Flower(Plant):
+    pass
+
+
+class Fruit (Plant):
+    def __init__(self, name, edible=True):
+        super().__init__(name)
+        self.edible = edible
+
+
+Fox = Predator('Лиса')
+Dog = Mammal('Маламут')
+Mak = Flower('Мак')
+Apple = Fruit('Яблоко')
+
+print(Fox.name)
+print(Mak.name)
+print(Fox.alive)
+print(Dog.fed)
+
+Fox.eat(Mak)
+Dog.eat(Apple)
+
+print(Fox.alive)
+print(Dog.fed)
+
